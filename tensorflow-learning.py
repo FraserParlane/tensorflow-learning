3d3986bfb0a4f0a9533da163a5de79b892d6d95f
Fraser Parlane <mail@fraserparlane.com>
2022-11-15 15:33:09 -0800

    CNN model stubbed.

--- tensorflow-learning.py
+++ tensorflow-learning.py
@@ -1,5 +1,6 @@
-# import tensorflow as tf
+import matplotlib.pyplot as plt
 from typing import Tuple
+import tensorflow as tf
 from tqdm import tqdm
 import urllib.request
 import pandas as pd
@@ -51,6 +52,39 @@ def get_data(
     return train_x, train_y, test_x, test_y
 
 
+def make_model() -> tf.keras.models.Sequential:
+    """
+    Make the CNN model for training
+    :return: The model.
+    """
+
+    # Make the model
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(
+        filters=32,
+        kernel_size=(3, 3),
+        activation='relu',
+        kernel_initializer='he_uniform',
+        input_shape=(28, 28, 1),
+    ))
+    model.add(tf.keras.layers.MaxPooling2D(
+        pool_size=(2, 2),
+    ))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(
+        units=100,
+        activation='relu',
+        kernel_initializer='he_uniform',
+    ))
+    model.add(tf.keras.layers.Dense(
+        units=10,
+        activation='softmax',
+    ))
+
+    # Return
+    return model
+
+
 def train_cnn(
 ) -> None:
     """
@@ -61,12 +95,15 @@ def train_cnn(
     # Get the training data from Keras. Cached locally after first load.
     # 60,000 in train, 10,000 in test. images are 28 x 28.
     train_x, train_y, test_x, test_y = get_data(use_cache=True)
-    print(train_x)
 
     # Normalize the x data to be between 0 and 1.
-    print('a')
+    train_x = train_x / 255
+    test_x = test_x / 255
+
+    # Make the CNN model
+    model = make_model()
+
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     train_cnn()
-
